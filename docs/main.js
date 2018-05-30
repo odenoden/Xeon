a127a3ba247dacc88ae9ebcd250c8611f5579f16
odenoden <fs209ueda@gmail.com>
2018-05-30 22:51:00 +0900

    XEM価格取得

--- docs/main.js
+++ docs/main.js
@@ -2,39 +2,29 @@ var app = new Vue({
     el: '#app',
 
     data: {
-        list: ['りんご', 'ばなな', 'いちご'],
+        XEM_Rate: 0,
         XEM_Price: 0,
-        message1: '',
-        message2: '',
-        show: true,
-        zaif_url: 'https://api.zaif.jp/api/1/ticker/xem_jpy',
-        result: '--'
+        xembook_url: 'http://13.113.193.148/xembook/lastprice2.json'
     },
 
-    mounted() {
-        axios.get(zaif_url)
-        .then(response => {this.result = response.data});
-      },
+    created () {
+        axios.get(this.xembook_url)
+        .then(function (response) {
+            app.XEM_Rate = Number(response.data.zaif)
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    },
 
-      methods: {
+    methods: {
 
         handleClick: function (message) {
             alert(message) // [object HTMLButtonElement]
         },
 
-        // GET request
-        getPosts(url) {
-            alert(url);
-            axios.get('https://api.zaif.jp/api/1/ticker/xem_jpy')
-            .then(function (response) {
-                alert('aaaa');
-              console.log(response);
-            })
-            .catch(function (error) {
-                alert(error.message);
-              console.log(error);
-            });
-            alert('test1');
+        getXEMPrice: function () {
+            alert('開発中・・・')
         }
     }
 })
